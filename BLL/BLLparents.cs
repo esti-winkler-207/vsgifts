40624e20ab256e630e2ec1cb53515779d796d556
esti-winkler-207 <77900987+esti-winkler-207@users.noreply.github.com>
2021-02-14 23:41:38 +0200

    fiveth commit

---
+++ BLL/BLLparents.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BLL
+{
+  public  class BLLparents
+    {
+        public static List<DTO.parents> addParent(DTO.parents p)
+        {
+            return CONVERTERS.Cparents.ConvertToDTOparentsList(
+                DAL.DALparents.addgroup(CONVERTERS.Cparents.ConvertToDALparents(p))
+                );
+
+        } 
+    }
+}
---
+++ WebAPI5/Controllers/ParentsController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace WebAPI5.Controllers
+{
+    [RoutePrefix("api/parents")]
+    public class ParentsController : ApiController
+    {
+        [HttpPost]
+        [Route("addParent")]
+        public IHttpActionResult addParent(DTO.parents p)
+        {
+            return Ok(BLL.BLLparents.addParent(p));
+        }
+    }
+}
